9412e1791dc986d5c470946fb429bc98965ef74e
malesnif <lmartino@wp.pl>
2023-01-07 17:02:27 +0100

    nowe pliki

---
+++ src/Lab12/Book.java
@@ -0,0 +1,11 @@
+package Lab12;
+
+public class Book {
+
+    String title;
+    String author;
+    String publisher;
+    String year;
+
+
+}
---
+++ src/Lab12/Library.java
@@ -0,0 +1,6 @@
+package Lab12;
+
+public class Library {
+    String[] Book = {""};
+
+}
